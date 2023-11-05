d69e818d8798f44618634b1b7f5f998018955067
divgive <divgive@gmail.com>
2023-11-05 21:38:06 +0300

    Final work for 2023/11/05

---
+++ src/app/fonts.tsx
@@ -0,0 +1,8 @@
+import { Raleway } from "next/font/google";
+
+export const raleway = Raleway({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-raleway",
+  display: "swap",
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+import { ReactNode } from "react";
+import { raleway } from "./fonts";
+import "./styles/variables.scss";
+import "./styles/reset.scss";
+import "./styles/global.scss";
+
+interface IRootLayout {
+  children: ReactNode;
+}
+
+export default function RootLayout({ children }: IRootLayout) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={raleway.className}>{children}</body>
     </html>
-  )
+  );
 }
